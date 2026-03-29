033099eddda6b27cfae5a11d8d0f72fb26766c27
vebgenix08 <ags@vebgenix.com>
2026-03-30 04:23:42 +0530

    Harden EC2 database bootstrap migrations

--- server/src/migrations/018_add_campus_type_enum_with_degree.sql
+++ server/src/migrations/018_add_campus_type_enum_with_degree.sql
@@ -8,14 +8,30 @@ BEGIN
   END IF;
 END $$;
 
+ALTER TABLE public.campuses
+  DROP CONSTRAINT IF EXISTS campuses_campus_type_check;
+
+ALTER TABLE public.students
+  DROP CONSTRAINT IF EXISTS students_campus_type_check;
+
 -- Convert campuses.campus_type -> CampusType
 ALTER TABLE public.campuses
   ALTER COLUMN campus_type TYPE "CampusType"
-  USING campus_type::text::"CampusType";
+  USING CASE UPPER(campus_type::text)
+    WHEN 'SCHOOL' THEN 'SCHOOL'::"CampusType"
+    WHEN 'PU' THEN 'PU'::"CampusType"
+    WHEN 'DEGREE' THEN 'DEGREE'::"CampusType"
+    ELSE 'SCHOOL'::"CampusType"
+  END;
 
 -- Convert students.campus_type -> CampusType
 ALTER TABLE public.students
   ALTER COLUMN campus_type TYPE "CampusType"
-  USING campus_type::text::"CampusType";
+  USING CASE UPPER(campus_type::text)
+    WHEN 'SCHOOL' THEN 'SCHOOL'::"CampusType"
+    WHEN 'PU' THEN 'PU'::"CampusType"
+    WHEN 'DEGREE' THEN 'DEGREE'::"CampusType"
+    ELSE 'SCHOOL'::"CampusType"
+  END;
 
 COMMIT;
