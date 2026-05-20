a97097f6c0a5ca8533c3fe841e7a21886ad74fd2
vebgenix08 <ags@vebgenix.com>
2026-05-20 16:11:09 +0530

    Remove puppeteer-core and chromium shims declaration file

--- apps/finance-service/src/shims.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'puppeteer-core' {
-  const puppeteer: any;
-  export default puppeteer;
-}
-
-declare module '@sparticuz/chromium' {
-  const chromium: any;
-  export default chromium;
-}
